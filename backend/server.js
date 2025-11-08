1cd890ad21eea75fada2d10bfc1ea60ee756c029
Rehmanvirk <rehmanvirk318@gmail.com>
2025-11-08 13:07:45 +0500

    edit port

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use('/api/generate', genRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
